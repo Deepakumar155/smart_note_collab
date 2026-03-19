dcc633e668de9b56d1e3b02d7dfd5cb7bd5b6661
DEEPAKUMAR D <deepak1552005@gmail.com>
2026-03-19 22:19:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: ['https://smart-note-collab.vercel.app', 'https://smart-note-collab.vercel.app', 'https://smart-note-collab.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 // Socket.IO Setup
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    origin: ['https://smart-note-collab.vercel.app', 'https://smart-note-collab.vercel.app', 'https://smart-note-collab.vercel.app'],
     methods: ["GET", "POST"]
   }
 });
